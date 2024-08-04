416ba440ca6c6e68fd09d32ab989e707dc6cfdbd
DanielMattos <dmattos@rapi.tech>
2024-08-04 17:04:43 -0500

    Parte 1

---
+++ soycalidad/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Prueba Tecnica-SOyCalidad",
+
+    'summary': """
+        Prueba Técnica
+    """,
+    'author': "Daniel Mattos R.",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'license': 'LGPL-3',
+    'depends': [
+        'point_of_sale',
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'soycalidad/static/src/xml/**/*',
+        ],
+    },
+}
